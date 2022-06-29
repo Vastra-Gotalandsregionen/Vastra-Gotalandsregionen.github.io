e9afd7606d3fdf6dc0a4b558a33d871a02ad935d
Marcus Österberg <m@tba.nu>
2022-06-29 09:59:30 +0200

    Samlingssida 2.0 - prototyp

---
+++ samlingssida-2.0/1-startsida_files/configs.php
@@ -0,0 +1,1 @@
+Piwik.HeatmapSessionRecording.configuration.assign({"heatmaps":[],"sessions":[],"idsite":"194","trackerid":"fcF1Zr"});
---
+++ samlingssida-2.0/2-kommuner_files/configs.php
@@ -0,0 +1,1 @@
+Piwik.HeatmapSessionRecording.configuration.assign({"heatmaps":[],"sessions":[],"idsite":"194","trackerid":"kjgZ7T"});
